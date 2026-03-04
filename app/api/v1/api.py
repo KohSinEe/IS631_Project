119638a906c248c6607620e6bf74923fc03978a6
Yu Hui Poon <yuhui@Yus-MacBook-Pro.local>
2026-03-04 13:51:38 +0800

    modify user_allergen endpoints aggregator

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -13,4 +13,4 @@ api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(items.router, prefix="/items", tags=["Items"])
 api_router.include_router(barcode.router, prefix="/barcode", tags=["Barcode Scanning"])
 api_router.include_router(recipes.router, prefix="/recipes", tags=["Recipes"])
-api_router.include_router(recipes.router, prefix="/users", tags=["User Allergens"]) #prefix is users to match the endpoint path for allergens
+api_router.include_router(user_allergen.router, prefix="/users", tags=["User Allergens"]) #prefix is users to match the endpoint path for allergens
